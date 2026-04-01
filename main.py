296098e8ae742119a6d0dbc4dda32e66181daa58
zimu <95072405+lqr-1169@users.noreply.github.com>
2026-04-01 10:29:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,40 +72,6 @@ lpkt.load("../EduKTM-main/examples/LPKT/ednetparams/lpkt2.params")
 _, auc, accuracy, rmse, r2 = lpkt.eval(test_data)
 print("auc: %.6f, accuracy: %.6f,  rmse: %.6f,  r2: %.6f" % (auc, accuracy, rmse, r2))
 
-
-
-# #assist2017
-# batch_size = 32
-# n_at = 1326
-# n_it = 2839
-# n_question = 102
-# n_exercise = 3162
-# seqlen = 500
-# d_k = 128
-# d_a = 50
-# d_e = 128
-# d_f = 3162
-# d_l = 1709
-# d_x = 3000
-# q_gamma = 0.03
-# p_gamma = 0.01
-# dropout = 0.2 
-
-# q_matrix = generate_q_matrix(
-#     '../EduKTM-main/data/anonymized_full_release_competition_dataset/problem2skill',
-#     n_question, n_exercise,
-#     q_gamma
-# )
-
-# p_matrix = generate_p_matrix('../EduKTM-main/examples/LPKT/dependency_dict_extend_pp_assist2017.txt', p_gamma)
-
-# dat = DATA(seqlen=seqlen, separate_char=',')
-# logging.getLogger().setLevel(logging.INFO)
-
-# train_data = dat.load_data('../EduKTM-main/data/assist2017train_va_te/train0.txt')
-# valid_data = dat.load_data('../EduKTM-main/data/assist2017train_va_te/valid0.txt')
-# test_data = dat.load_data('../EduKTM-main/data/assist2017train_va_te/test.txt')
-
 lpkt = LPKT(n_at, n_it, n_exercise, n_question, d_a, d_e, d_k, d_l, d_f, q_matrix, p_matrix, batch_size, dropout)
 lpkt.train(train_data, valid_data, epoch=30, lr=0.003, lr_decay_step=10)
 lpkt.load("../EduKTM-main/examples/LPKT/2017params/lpkt3.params")
